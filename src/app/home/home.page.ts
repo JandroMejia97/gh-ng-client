d29db0e67b976a74062647663f76030a5c17008b
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-09-22 15:08:40 -0300

    Better performance * Adding virtual scroll * Infinite scrolling * Refresher

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SearchbarCustomEvent } from '@ionic/core';
+import { IonInfiniteScroll, IonRefresher } from '@ionic/angular';
+import { SearchbarCustomEvent, ScrollCustomEvent, RefresherCustomEvent } from '@ionic/core';
 
 import { Order, Sort } from '../models/search-params.model';
 import { SearchResult } from '../models/search-result.model';
@@ -12,10 +13,18 @@ import { UserService } from '../services/user.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild(IonInfiniteScroll)
+  infiniteScroll: IonInfiniteScroll;
+  @ViewChild(IonRefresher)
+  refresher: IonRefresher;
   searchResult: SearchResult | null = null;
   sortBy: Sort | null = null;
+  sortOptions: Sort[] = ['followers', 'repositories', 'joined'];
   orderBy: Order = 'desc';
+  orderOptions: Order[] = ['asc', 'desc'];
   search: string | null = null;
+  itemsPerPage = 30;
+  private currentPage = 1;
 
   constructor(
     private router: Router,
@@ -37,7 +46,7 @@ export class HomePage implements OnInit {
     }
   }
 
-  async onSearchChange(event: SearchbarCustomEvent) {
+  async onSearchChange(event: Partial<SearchbarCustomEvent>): Promise<void> {
     const { value } = event.detail;
     if (value) {
       this.search = value;
@@ -45,14 +54,72 @@ export class HomePage implements OnInit {
         search: value,
         sort: this.sortBy,
         order: this.orderBy,
+        perPage: this.itemsPerPage,
       };
       this.router.navigate([], {
         queryParams,
         queryParamsHandling: 'merge',
       });
-      this.userService.getUsers(queryParams).subscribe((searchResult) => {
-        this.searchResult = searchResult;
+      this.userService.getUsers(queryParams).subscribe({
+        next: (searchResult) => {
+          this.searchResult = searchResult;
+          this.infiniteScroll.disabled = false;
+          this.refresher.disabled = false;
+        },
+        error: (error) => {
+          this.infiniteScroll.disabled = true;
+          this.refresher.disabled = true;
+          if (error.status === 404) {
+            this.searchResult.items = [];
+          }
+        },
+        complete: () => {
+          this.refresher.complete();
+        }
       });
+    }
+  }
+
+  async orderOrSortChanged(sort: Sort, order: Order): Promise<void> {
+    this.sortBy = sort;
+    this.orderBy = order;
+    this.onSearchChange({
+      detail: {
+        value: this.search,
+      },
+    } as SearchbarCustomEvent);
+  }
+
+  onSearchClear(): void {
+    this.search = null;
+    this.searchResult = null;
+    this.router.navigate([], {
+      queryParams: {},
+    });
+  }
+
+  async onScroll(): Promise<void> {
+    if (this.currentPage * this.itemsPerPage < this.searchResult?.totalCount) {
+      this.currentPage++;
+      const queryParams = {
+        search: this.search,
+        sort: this.sortBy,
+        order: this.orderBy,
+        perPage: this.itemsPerPage,
+        page: this.currentPage,
+      };
+      this.userService.getUsers(queryParams).subscribe({
+        next: (searchResult) => {
+          this.searchResult.items = this.searchResult?.items.concat(
+            searchResult.items
+          );
+        },
+        complete: () => {
+          this.infiniteScroll.complete();
+        }
+      });
+    } else {
+      this.infiniteScroll.disabled = true;
     }
   }
 }
